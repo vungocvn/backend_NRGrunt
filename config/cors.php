1926854a615985395ad663fd2ebcb2831db33630
vungocvn <ngocred14022004@gmail.com>
2025-03-28 15:47:23 +0700

    commit_cors

--- config/cors.php
+++ config/cors.php
@@ -2,8 +2,11 @@
 
 return [
     'paths' => ['*'],
-    'allowed_methods' => ["GET", 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*'],
+    // ["GET", 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     'allowed_origins' => ['*'],
-    'allowed_headers' => ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization'],
+    'allowed_headers' => ['*'],
+    // ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization'],
+    'exposed_headers' => [],
     'supports_credentials' => false,
 ];
